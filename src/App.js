abee13f4db82f7a1defdbb7e54fa88f5febc4b3e
Meet Guleria <meetguleria94@gmail.com>
2023-08-28 10:40:33 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Skills from './Components/Skills';
 import Projects from './Components/Projects';
 import Footer from './Components/Footer';
 
-import { Analytics } from '@vercel/analytics';
 function App() {
   return (
     <div className="App">
